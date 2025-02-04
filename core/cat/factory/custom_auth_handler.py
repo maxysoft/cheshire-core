5611038f06de2d1c2db6c0c63f8706a9fe159846
Tomas <1979739+maxysoft@users.noreply.github.com>
2025-02-04 23:37:28 +0000

    Update custom_auth_handler.py
    Attempt to fix session token

--- core/cat/factory/custom_auth_handler.py
+++ core/cat/factory/custom_auth_handler.py
@@ -67,22 +67,17 @@ class BaseAuthHandler(ABC):  # TODOAUTH: pydantic model?
 # Core auth handler, verify token on local idp
 class CoreAuthHandler(BaseAuthHandler):
 
-    def authorize_user_from_jwt(
-        self, token: str, auth_resource: AuthResource, auth_permission: AuthPermission
-    ) -> AuthUserInfo | None:
+    def authorize_user_from_jwt(self, token: str, auth_resource: AuthResource, auth_permission: AuthPermission) -> AuthUserInfo | None:
         try:
-            # decode token
             payload = jwt.decode(
                 token,
                 get_env("CCAT_JWT_SECRET"),
                 algorithms=[get_env("CCAT_JWT_ALGORITHM")],
             )
 
-            # get user from DB
             users = get_users()
             if payload["sub"] in users:
                 user = users[payload["sub"]]
-                # TODOAUTH: permissions check should be done in a method
                 if auth_resource in user["permissions"].keys() and \
                         auth_permission in user["permissions"][auth_resource]:
                     return AuthUserInfo(
@@ -95,78 +90,54 @@ class CoreAuthHandler(BaseAuthHandler):
         except Exception as e:
             log.error(f"Could not auth user from JWT: {e}")
 
-        # do not pass
         return None
 
-    def authorize_user_from_key(
-            self,
-            protocol: Literal["http", "websocket"],
-            user_id: str,
-            api_key: str,
-            auth_resource: AuthResource,
-            auth_permission: AuthPermission,
-    ) -> AuthUserInfo | None:
+    def authorize_user_from_key(self, protocol: Literal["http", "websocket"], api_key: str, auth_resource: AuthResource, auth_permission: AuthPermission) -> AuthUserInfo | None:
         http_key = get_env("CCAT_API_KEY")
         ws_key = get_env("CCAT_API_KEY_WS")
 
         if not http_key and not ws_key:
             return AuthUserInfo(
-                id=user_id,
-                name=user_id,
+                id="unknown",
+                name="unknown",
                 permissions=get_full_permissions()
             )
 
         if protocol == "websocket":
-            return self._authorize_websocket_key(user_id, api_key, ws_key)
+            return self._authorize_websocket_key(api_key, ws_key)
         else:
-            return self._authorize_http_key(user_id, api_key, http_key)
+            return self._authorize_http_key(api_key, http_key)
 
-    def _authorize_http_key(self, user_id: str, api_key: str, http_key: str) -> AuthUserInfo | None:
-
-        # HTTP API key match -> allow access with full permissions
+    def _authorize_http_key(self, api_key: str, http_key: str) -> AuthUserInfo | None:
         if api_key == http_key:
             return AuthUserInfo(
-                id=user_id,
-                name=user_id,
+                id="unknown",
+                name="unknown",
                 permissions=get_full_permissions()
             )
-
-        # No match -> deny access
         return None
 
-    def _authorize_websocket_key(self, user_id: str, api_key: str, ws_key: str) -> AuthUserInfo | None:
-
-        # WebSocket API key match -> allow access with base permissions
+    def _authorize_websocket_key(self, api_key: str, ws_key: str) -> AuthUserInfo | None:
         if api_key == ws_key:
             return AuthUserInfo(
-                id=user_id,
-                name=user_id,
+                id="unknown",
+                name="unknown",
                 permissions=get_base_permissions()
             )
-
-        # No match -> deny access
         return None
 
     def issue_jwt(self, username: str, password: str) -> str | None:
-        # authenticate local user credentials and return a JWT token
-
-        # brutal search over users, which are stored in a simple dictionary.
-        # waiting to have graph in core to store them properly
-        # TODOAUTH: get rid of this shameful loop
         users = get_users()
         for user_id, user in users.items():
             if user["username"] == username and check_password(password, user["password"]):
-                # TODOAUTH: expiration with timezone needs to be tested
-                # using seconds for easier testing
                 expire_delta_in_seconds = float(get_env("CCAT_JWT_EXPIRE_MINUTES")) * 60
                 expires = datetime.now(utc) + timedelta(seconds=expire_delta_in_seconds)
-                # TODOAUTH: add issuer and redirect_uri (and verify them when a token is validated)
 
                 jwt_content = {
-                    "sub": user_id,                      # Subject (the user ID)
-                    "username": username,                # Username
-                    "permissions": user["permissions"],  # User permissions
-                    "exp": expires                       # Expiry date as a Unix timestamp
+                    "sub": user_id,
+                    "username": username,
+                    "permissions": user["permissions"],
+                    "exp": expires
                 }
                 return jwt.encode(
                     jwt_content,
