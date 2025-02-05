8aa9696375b4b6fea31344c95cca539d206d1891
maxysoft <1979739+maxysoft@users.noreply.github.com>
2025-02-06 00:33:46 +0100

    fixed routers

--- core/cat/startup.py
+++ core/cat/startup.py
@@ -81,13 +81,19 @@ if cors_enabled == "true":
 cheshire_cat_api.include_router(base.router, tags=["Home"])
 cheshire_cat_api.include_router(auth.router, tags=["User Auth"], prefix="/auth")
 cheshire_cat_api.include_router(users.router, tags=["Users"], prefix="/users")
-cheshire_cat_api.include_router(settings.router, tags=["Settings"], prefix="/settings"])
-cheshire_cat_api.include_router(llm.router, tags=["Large Language Model"], prefix="/llm")
-cheshire_cat_api.include_router(embedder.router, tags=["Embedder"], prefix="/embedder"])
-cheshire_cat_api.include_router(plugins.router, tags=["Plugins"], prefix="/plugins"])
+cheshire_cat_api.include_router(settings.router, tags=["Settings"], prefix="/settings")
+cheshire_cat_api.include_router(
+    llm.router, tags=["Large Language Model"], prefix="/llm"
+)
+cheshire_cat_api.include_router(embedder.router, tags=["Embedder"], prefix="/embedder")
+cheshire_cat_api.include_router(plugins.router, tags=["Plugins"], prefix="/plugins")
 cheshire_cat_api.include_router(memory_router, prefix="/memory")
-cheshire_cat_api.include_router(upload.router, tags=["Rabbit Hole"], prefix="/rabbithole"])
-cheshire_cat_api.include_router(auth_handler.router, tags=["AuthHandler"], prefix="/auth_handler"])
+cheshire_cat_api.include_router(
+    upload.router, tags=["Rabbit Hole"], prefix="/rabbithole"
+)
+cheshire_cat_api.include_router(
+    auth_handler.router, tags=["AuthHandler"], prefix="/auth_handler"
+)
 cheshire_cat_api.include_router(websocket.router, tags=["Websocket"])
 
 # mount static files
